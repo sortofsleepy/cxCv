8ffed3a9dee569960ca12fb39593570ddd7b512b
Joseph Chow <sortofsleepy@gmail.com>
2014-02-17 12:24:01 -0800

    Added basic face-tracking implementation

--- FaceTracking/FaceTracker.h
+++ FaceTracking/FaceTracker.h
@@ -33,9 +33,45 @@ public:
 
     }
     
-    void update(){}
+    void update(Surface image){
+    	const int calcScale = 6; // calculate the image at half scale
+        cv::Mat img(ci::toOcv(Channel(image)));
+     
+        
+        // create a grayscale copy of the input image
+        cv::Mat grayCameraImage( cxCv::toGray(img) );
+        
+        // scale it to half size, as dictated by the calcScale constant
+        int scaledWidth = image.getWidth() / calcScale;
+        int scaledHeight = image.getHeight() / calcScale;
+        cv::Mat smallImg( scaledHeight, scaledWidth, CV_8UC1 );
+        cv::resize( grayCameraImage, smallImg, smallImg.size(), 0, 0, cv::INTER_LINEAR );
+        
+        // equalize the histogram
+        cv::equalizeHist( smallImg, smallImg );
+        
+        // clear out the previously deteced faces & eyes
+        mFaces.clear();
+ 
+        
+        // detect the faces and iterate them, appending them to mFaces
+        vector<cv::Rect> faces;
+        mFaceCascade.detectMultiScale( smallImg, faces );
+        for( vector<cv::Rect>::const_iterator faceIter = faces.begin(); faceIter != faces.end(); ++faceIter ) {
+            Rectf faceRect( fromOcv( *faceIter ) );
+            faceRect *= calcScale;
+            mFaces.push_back( faceRect );
+            
+        }
+
+    }
     
-    void draw(){}
+    void draw(){
+    	// draw the faces as transparent yellow rectangles
+        gl::color( ColorA( 1, 1, 0, 0.45f ) );
+        for( vector<Rectf>::const_iterator faceIter = mFaces.begin(); faceIter != mFaces.end(); ++faceIter )
+		gl::drawSolidRect( *faceIter );
+    }
 };
 
 #endif
